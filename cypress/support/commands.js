4e94027742223aa180718c79f71cffb01695fce2
Shlomo Bluth <lomibluth@gmail.com>
2020-12-21 07:19:53 +0200

    add commit

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,16 +1,40 @@
-
-Cypress.Commands.add('changeLanguage',()=>{
-    cy.get('body').then(elem => {
-        let classAttr = elem.attr("class"); 
-        //cy.log(score);
-        // etc...
-        if(classAttr=='he')
-      {
-        cy.get('a').contains(/^English$/).click();
-      }else {
-        cy.get('a').contains(/^עברית$/).click();
+Cypress.Commands.add('homePageSection',(language,url,numberOfButton)=>{
+  cy.setLanguageMode(language)
+    let count=0
+    cy.get('div[class*="nakdan-auto"]').children().each(($el)=>{
+      if($el.is('a')){
+        count++
+        cy.get($el).should('have.attr', 'target', '_blank').
+        and('have.attr', 'href',url)
+      } else if($el.is('div[class*="product-description"]')){
+        cy.get($el).children('a').not('[id="nakdanDemoLink"]').then(()=>{
+          count++
+        })
+        .should('have.attr', 'target', '_blank').
+        and('have.attr', 'href',url)
       }
+    }).then(()=>{
+      expect(count).equal(numberOfButton)
     })
+})
+
+
+
+Cypress.Commands.add('setLanguageMode',(language)=>{
+  cy.get('body').then(elem => {
+    let languageMode
+    if(language=='Hebrew'){
+      languageMode='he'
+    }else if(language=='English'){
+      languageMode=''
+    }
+    let classAttr = elem.attr("class");
+    if(classAttr!=languageMode)
+    {
+      cy.get('a').contains(/^English$/||/^עברית$/).click();
+    }
+    cy.get('body').should('have.attr','class',languageMode)
+  })
 })
 
 Cypress.Commands.add('clearAllCookies', () => {
