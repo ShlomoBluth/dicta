dd901e2594097778ca06552f4ad7be5ed0b93dbf
Shlomo Bluth <lomibluth@gmail.com>
2020-12-21 08:39:07 +0200

    add commit

---
+++ cypress/integration/nakdanProSectionInHomePage.js
@@ -0,0 +1,42 @@
+/// <reference types="cypress"/>
+
+//run tests on the nakdan pro section in dicta home page some in hebrew mode and english mode
+//some will succeed and some will fail
+
+describe('NakdanProSectionInHomePage',()=>{
+
+  beforeEach(() => {
+    cy.visit('https://dicta.org.il/')
+  })
+
+//Need to go through all the cookies in the firefox browser and clear after each test otherwise 
+//a error will pop up and the tests will not continue to run 
+  afterEach(() => {
+    cy.clearAllCookies();
+  })
+
+  it('Links in the nakdan-pro section in hebrew mode have 3 nakdanpro urls and open in a new tab',()=>{
+    cy.homePageSection('nakdan-pro','Hebrew','https://nakdanpro.dicta.org.il/',2)
+  })
+
+  it('Links in the nakdan-auto section in english mode have 3 nakdanpro urls and open in a new tab',()=>{
+    cy.homePageSection('nakdan-pro','English','https://nakdanpro.dicta.org.il/',2)
+  })
+
+  it('Links in the nakdan-auto section in hebrew mode have 4 nakdanpro urls and open in a new tab',()=>{
+    cy.homePageSection('nakdan-pro','Hebrew','https://nakdanpro.dicta.org.il/',3)
+  })
+
+  it('Links in the nakdan-auto section in english mode have 4 nakdanpro urls and open in a new tab',()=>{
+    cy.homePageSection('nakdan-pro','English','https://nakdanpro.dicta.org.il/',3)
+  })
+
+  it('Links in the nakdan-auto section in hebrew mode have wrong nakdanpro url and open in a new tab',()=>{
+    cy.homePageSection('nakdan-pro','Hebrew','https://nnakdanpro.dicta.org.il/',2)
+  })
+
+  it('Links in the nakdan-pro section in english mode have wrong nakdanpro url and open in a new tab',()=>{
+    cy.homePageSection('nakdan-pro','English','https://nnakdanpro.dicta.org.il/',2)
+  })
+
+})   
